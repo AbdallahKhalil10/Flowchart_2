f4c52275fbaf3559a8d57d2b6e5944984e1c1aca
Abdallah Ahmed <142527303+AbdallahAhmed7@users.noreply.github.com>
2023-12-24 00:09:58 +0200

    Conditional_statement

---
+++ Statements/Conditional.cpp
@@ -0,0 +1,67 @@
+#include "Conditional.h"
+#include <sstream>
+
+using namespace std;
+
+Conditional::Conditional(Point Lcorner, string LeftHS, string Op, string RightHS)
+{
+
+	// Note: The LeftHS and RightHS should be validated inside (AddValueAssign) action
+	//       before passing it to the constructor of ValueAssign
+	LHS = LeftHS;
+    OP = Op;
+	RHS = RightHS;
+
+	UpdateStatementText();
+
+	LeftCorner = Lcorner;
+	RightCorner.x = Lcorner.x + UI.ASSGN_WDTH;
+	RightCorner.y = Lcorner.y;
+
+	pOutConn = NULL;	//No connectors yet
+
+	Inlet.x = LeftCorner.x + UI.ASSGN_WDTH / 2;
+	Inlet.y = LeftCorner.y;
+
+	Outlet.x = Inlet.x;
+	Outlet.y = LeftCorner.y + UI.ASSGN_HI;
+}
+
+void Conditional::setLHS(const string& L)
+{
+	LHS = L;
+	UpdateStatementText();
+}
+
+void Conditional::setRHS(const string& R)
+{
+	RHS = R;
+	UpdateStatementText();
+}
+
+void Conditional::setOperator(const string& Op)
+{
+
+	if (Op == "==" || Op == "!=" || Op == "<" || Op == ">" || Op == "<=" || Op == ">=")
+		OP = Op;
+
+	UpdateStatementText();
+}
+
+
+void Conditional::Draw(Output* pOut) const
+{
+	//Call Output::DrawAssign function to draw conditional statement 	
+	pOut->DrawConditionalStatement(RightCorner, UI.ASSGN_WDTH, UI.ASSGN_HI, Text, Selected);
+
+}
+
+
+//This function should be called when LHS or RHS changes
+void Conditional::UpdateStatementText()
+{
+	//Build the statement text: Left handside then equals then right handside
+	ostringstream T;
+	T << LHS << OP << RHS;
+	Text = T.str();
+}
